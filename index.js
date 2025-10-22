f220405a4858f4cb5ad74b716ffd603348a751e1
MdSalman2022 <mehedi.salman102@gmail.com>
2025-10-22 12:50:11 +0600

    added trust proxy

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ import { securityHeaders } from "./middleware/security.js";
 import { initializeAuth } from "./config/auth.js";
 
 const app = express();
+app.set("trust proxy", true);
 const port = process.env.PORT || 5000;
 
 (async () => {
